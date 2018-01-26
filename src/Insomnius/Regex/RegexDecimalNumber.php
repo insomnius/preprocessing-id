2c8b99d11353142458b9580f33435be574553867
Insomnius <aliana.arib@gmail.com>
2018-01-26 17:27:08 +0700

    Fixing regex decimal number

--- src/Insomnius/Regex/RegexDecimalNumber.php
+++ src/Insomnius/Regex/RegexDecimalNumber.php
@@ -9,15 +9,17 @@ class RegexDecimalNumber implements RegexInterface
     public function regex($word)
     {
         $matches  = null;
-        $contains = preg_match('/-?\d+(,\d+)*(\.\d+(e\d+)?)?/', $word, $matches);
+        
+        $patern   = '/-?\d+(,\d+)*(\.\d+(e\d+)?)?/';
+        $contains = preg_match_all($patern, $word, $matches);
 
         $detail     = new Detail();
         $detail->groupProcess   = 'Regex';
         $detail->process        = 'Regex';
         $detail->class          = get_class($this);
         $detail->detail         = 'Get rid of all decimal number.';
-        $detail->match          = $matches;
-        $detail->wordAfterProcess   = strtolower($word);
+        $detail->match          = $matches[0];
+        $detail->wordAfterProcess   = preg_replace($patern, '', $word);
         $detail->wordBeforeProcess  = $word;
         
         return $detail;
